2eae736e4451c5d67eca3cc91af529d1279f82be
Jacob Helwig <jacob@technosorcery.net>
2017-06-06 10:30:35 -0700

    Ability to list ECR repositories

--- README.md
+++ README.md
@@ -6,3 +6,27 @@
 
 ecr-cleaner is released under the MIT license.
 See `LICENSE` for details.
+
+## Supported platforms
+
+At this time, ecr-cleaner has only been developed for and tested on OS X. Support for Linux is planned.
+
+## Installing dependencies
+
+ecr-cleaner requires the following other programs to be available on your system:
+
+* [OpenSSL](https://www.openssl.org/)
+
+### OS X
+
+All the dependencies can be installed with [Homebrew](http://brew.sh/):
+
+```
+brew install openssl
+```
+
+To use the Homebrew-installed OpenSSL, prefix the `cargo build` command (in the section on build from source below) with:
+
+``` bash
+OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,140 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate clap;
+extern crate hyper;
+extern crate rusoto_core;
+extern crate rusoto_ecr;
+#[macro_use] extern crate log;
+extern crate env_logger;
+
+use std::borrow::Borrow;
+use std::error::Error;
+use std::str::FromStr;
+
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
+use hyper::client::Client;
+
+use rusoto_core::{ChainProvider, ProfileProvider, Region};
+use rusoto_ecr::{DescribeRepositoriesRequest, Ecr, EcrClient, ImageIdentifierList, ListImagesRequest, RepositoryList};
+use rusoto_core::default_tls_client;
+
+fn build_cli() -> App<'static, 'static> {
+    app_from_crate!(", ")
+        .setting(AppSettings::SubcommandRequired)
+        .arg(Arg::with_name("region")
+             .short("r")
+             .long("region")
+             .help("AWS region to operate in")
+             .default_value("us-east-1")
+             .validator(validate_region)
+        )
+        .arg(Arg::with_name("profile")
+             .long("profile")
+             .help("AWS credentials profile name")
+             .takes_value(true)
+        )
+        .subcommand(SubCommand::with_name("list")
+                    .about("List ECR repositories or their contents")
+                    .arg(Arg::with_name("repository")
+                         .help("ECR repository to list the contents of")))
 }
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let matches = build_cli().get_matches();
+
+    let mut profile_provider = ProfileProvider::new().unwrap();
+    if let Some(p) = matches.value_of("profile") {
+        println!("Setting AWS credentials profile to: {}", p);
+        profile_provider.set_profile(p);
+    }
+    let chain_provider = ChainProvider::with_profile_provider(profile_provider);
+
+    let region = Region::from_str(matches.value_of("region").unwrap()).unwrap();
+    info!("Running with region: {}", region);
+
+    let ecr_client = EcrClient::new(default_tls_client().unwrap(), chain_provider, region);
+    match matches.subcommand() {
+        ("list", Some(sub_m)) => { list_subcommand(sub_m, ecr_client); },
+        _                     => unreachable!(),
+    }
+}
+
+fn validate_region(region: String) -> Result<(), String> {
+    match Region::from_str(region.as_str()) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.description().into()),
+    }
+}
+
+fn list_subcommand(arg_matches: &ArgMatches, ecr_client: EcrClient<ChainProvider, Client>) {
+    if let Some(repo_name) = arg_matches.value_of("repository") {
+        println!("Listing images in {}:", repo_name);
+        list_repository_images(ecr_client, repo_name.to_string());
+    } else {
+        list_repositories(ecr_client);
+    }
+}
+
+fn list_repositories(ecr_client: EcrClient<ChainProvider, Client>) {
+    if let Some(mut repositories) = get_repository_list(ecr_client, DescribeRepositoriesRequest::default()) {
+        println!("Repository Name\t\tURI");
+        repositories.sort_by(|a, b| {
+            let a_name = match a.repository_name.borrow() {
+                &Some(ref n) => n.clone(),
+                &None => "n/a".to_string(),
+            };
+            let b_name = match b.repository_name.borrow() {
+                &Some(ref n) => n.clone(),
+                &None => "n/a".to_string(),
+            };
+            a_name.cmp(&b_name)
+        });
+
+        for repository in repositories.iter() {
+            debug!("Repository: {:?}", repository);
+            let repository_name = match repository.repository_name.borrow() {
+                &Some(ref n) => n.clone(),
+                &None => "n/a".to_string(),
+            };
+            let repository_uri = match repository.repository_uri.borrow() {
+                &Some(ref n) => n.clone(),
+                &None => "n/a".to_string(),
+            };
+            println!("{}\t\t{}", repository_name, repository_uri);
+        }
+    }
+}
+
+fn get_repository_list(ecr_client: EcrClient<ChainProvider, Client>, request: DescribeRepositoriesRequest) -> Option<RepositoryList> {
+    match ecr_client.describe_repositories(&request) {
+        Ok(response) => {
+            debug!("Got a response!");
+            debug!("Response {:?}", response);
+            let mut repositories = match response.repositories {
+                Some(repos) => repos,
+                None => return None,
+            };
+
+            if let Some(next_token) = response.next_token {
+                let new_request = DescribeRepositoriesRequest { next_token: Some(next_token), max_results: request.max_results, registry_id: request.registry_id, repository_names: request.repository_names };
+                if let Some(mut more_repos) = get_repository_list(ecr_client, new_request) {
+                    repositories.append(&mut more_repos);
+                }
+            }
+
+            return Some(repositories);
+        },
+        Err(e) => {
+            println!("Could not list repositories: {}", e.description());
+            None
+        }
+    }
+}
+
+fn list_repository_images(ecr_client: EcrClient<ChainProvider, Client>, repo_name: String) {
+}
+
+// fn get_repository_image_list(ecr_client: EcrClient<ChainProvider, Client>, request: ListImagesRequest) -> Option<ImageIdentifierList> {
+// }
